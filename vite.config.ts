8447934992d2cd5138ddbe8257901e6731f55e69
Abhigyan Rastogi <abhigyanrastogi0@gmail.com>
2026-05-12 12:54:31 +0530

    update base url for the project

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projects/ping-pong/',
   plugins: [
     react(),
     federation({
