c5e12c442a6b7ebe2deca449903387b061951489
Abhigyan Rastogi <abhigyanrastogi0@gmail.com>
2026-05-12 20:01:58 +0530

    micro front end daddy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { GameContainer } from './components/GameContainer'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { federation } from '@module-federation/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projects/ping-pong/',
+  base: '/',
+  preview: {
+    port: 5174,
+    cors: true,
+  },
   plugins: [
     react(),
     federation({
       name: 'ping_pong_app',
       filename: 'remoteEntry.js',
+      manifest: true,
       exposes: {
         './PingPongApp': './src/App.tsx',
       },
-      shared: ['react', 'react-dom']
     })
   ],
 })
