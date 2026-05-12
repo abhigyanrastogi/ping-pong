1747426585c3f215bb86030bc8b11350629e667f
Abhigyan Rastogi <abhigyanrastogi0@gmail.com>
2026-05-12 11:53:13 +0530

    exposing app so it can be used in portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'ping_pong_app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './PingPongApp': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
 })
